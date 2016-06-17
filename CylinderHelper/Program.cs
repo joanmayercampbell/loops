e8fabd8718df9214b96af877d9868b3d3bacda0b
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-06-17 09:49:58 -0400

    First commit with exception handling

--- CylinderHelper/Program.cs
+++ CylinderHelper/Program.cs
@@ -8,34 +8,72 @@ namespace CylinderHelper
 {
     class Program
     {
+        static double GetNumberFromUser()
+        {
+            double userNumber = 0;
+            bool numberEntered = false;
+
+            while (!numberEntered)
+            {
+                try
+                {
+                    string userResponse = Console.ReadLine();
+                    userNumber = Convert.ToDouble(userResponse);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Please enter a number. Try again!");
+                }
+
+                numberEntered = true;
+            }
+        }
+         
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the radius ? ");
-            double radius = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The radius is " + radius);
+            bool end = true;
+            string userinput = "not done";
 
-            Console.WriteLine("What is the height ? ");
-            double height = Convert.ToDouble(Console.ReadLine());
-
-            //Volume = radius^2 * height * pi
-            // radius ^ 2 is radius * radius
-
-            double pi = 3.14159;
-            if (radius > 0 && height > 0)
+            while (end)
             {
-                double volume = (radius * radius) * height * pi;
-                Console.WriteLine("The volume is " + volume);
+                //inital statement
+                Console.WriteLine("Calculate new cylinder values ");
 
-                // surface area = 2*pi*radius*height + 2*pi*radius^2
-                double surfaceArea = (pi * 2 * radius * height) + (2 * pi * radius * radius);
-                Console.WriteLine("the surface area = " + surfaceArea);
-            }
-            else
-            {
-                Console.WriteLine("You can't have a negative");
-            }
+                Console.WriteLine("What is the radius ? ");
+                
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("The radius is " + radius);
 
-            Console.ReadLine();
+                Console.WriteLine("What is the height ? ");
+                double height = Convert.ToDouble(Console.ReadLine());
+
+                //Volume = radius^2 * height * pi
+                // radius ^ 2 is radius * radius
+
+                double pi = 3.14159;
+                if (radius > 0 && height > 0)
+                {
+                    double volume = (radius * radius) * height * pi;
+                    Console.WriteLine("The volume is " + volume);
+
+                    // surface area = 2*pi*radius*height + 2*pi*radius^2
+                    double surfaceArea = (pi * 2 * radius * height) + (2 * pi * radius * radius);
+                    Console.WriteLine("the surface area = " + surfaceArea);
+                }
+                else
+                {
+                    Console.WriteLine("You can't have a negative");
+                }
+
+                Console.WriteLine("Enter \"done\" to exit");
+                userinput = Console.ReadLine();
+
+                if (userinput.ToLower() == "done")
+                {
+                    end = false;
+                }
+               
+            }
         }
     }
 }
